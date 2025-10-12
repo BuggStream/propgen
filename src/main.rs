b37649f2293371c24b0c5066ea7e8f01e970b873
Jim van Vliet <j.i.m.vanvliet@student.tudelft.nl>
2025-10-12 19:56:17 +0200

    Current progress rust analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,58 @@
+use std::env;
 use std::error::Error;
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use std::sync::LazyLock;
+use ra_ap_hir::Crate;
+use ra_ap_ide::AnalysisHost;
+use ra_ap_ide_db::base_db::SourceDatabase;
+use ra_ap_load_cargo::{load_workspace, LoadCargoConfig, ProcMacroServerChoice};
+use ra_ap_paths::{AbsPathBuf, Utf8PathBuf};
+use ra_ap_project_model::{CargoConfig, ProjectManifest, ProjectWorkspace, RustLibSource};
 use generate_put_pbt::parse_tests;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input_directory = Path::new(".");
-    let mut rust_files = Vec::new();
+    let cargo_config = CargoConfig {
+        sysroot: Some(RustLibSource::Discover),
+        all_targets: true,
+        set_test: false,
+        ..Default::default()
+    };
 
-    source_files(input_directory, &mut rust_files)?;
-    parse_tests(&rust_files)?;
+    let src_path = Utf8PathBuf::from_str("/home/jim/projects/thesis/hello")?;
+
+    let path = AbsPathBuf::assert(src_path);
+    let manifest = ProjectManifest::discover_single(&path)?;
+
+    let progress = |s| {
+        println!("{}", s);
+    };
+
+    let mut workspace = ProjectWorkspace::load(manifest, &cargo_config, &progress)?;
+    let load_cargo_config = LoadCargoConfig {
+        load_out_dirs_from_check: true,
+        with_proc_macro_server: ProcMacroServerChoice::Sysroot,
+        prefill_caches: false,
+    };
+
+    let (db, vfs, _proc_macro) =
+        load_workspace(workspace.clone(), &cargo_config.extra_env, &load_cargo_config)?;
+
+    let host = AnalysisHost::with_database(db);
+    let db = host.raw_database();
+
+    let krates = Crate::all(db);
+
+    let source_roots: Vec<_> = krates
+        .iter()
+        .cloned()
+        .map(|krate| db.file_source_root(krate.root_file(db)).source_root_id(db))
+        .collect();
+
+    for krate in krates {
+        println!("{:?}", krate.root_module().declarations(db));
+    }
 
     Ok(())
 }
